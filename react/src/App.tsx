ae20a027de4a5952d6b94956e77cd58d0654e32e
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-09-27 13:49:14 +0530

    learned about lazy loading and useReducer

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { lazy, Suspense } from 'react';
 import './App.css'
+import React from 'react'
+const RandomItem = lazy(() => import("./components/random"))
+interface Param {
+  value: number
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const today = React.useMemo(() => new Date(), []);
+  const inital: Param = { value: 100 }
+
+
+  function reducerji(value: Param) {
+    return { ...value, value: value.value + 100 };
+
+  }
+
+
+
+  const [token, setToken] = React.useReducer(reducerji, inital)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>{today.toString()}</p>
+        <p>{token.value}</p>
+        <button onClick={setToken}>
+
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <Suspense fallback={<div><h1>naamskar there </h1></div>}>
+          <RandomItem />
+        </Suspense>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
